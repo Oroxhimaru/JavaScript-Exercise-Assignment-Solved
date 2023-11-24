a6fc4b9280f15c810b1c72b3309e62b39621e012
Oroxhimaru <hm51562@gmail.com>
2023-11-25 00:14:19 +0500

    loops

---
+++ js exer(18-20_FORLoops)/loops.js
@@ -0,0 +1,8 @@
+/* 1. Write a program to display the message “Hello World” 5 times 
+in your browser using for loop. */
+
+for (let index = 0; index < 5; index++) {
+    console.log('Hello World');  
+}
+
+
