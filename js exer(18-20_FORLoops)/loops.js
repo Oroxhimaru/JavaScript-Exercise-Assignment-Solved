fbbc6f2d310943217f4eb6af3d8c0f7aaa870503
Oroxhimaru <hm51562@gmail.com>
2023-11-25 22:20:20 +0500

    loops work

--- js exer(18-20_FORLoops)/loops.js
+++ js exer(18-20_FORLoops)/loops.js
@@ -5,4 +5,26 @@ for (let index = 0; index < 5; index++) {
     console.log('Hello World');  
 }
 
+/* 2. Write a program to print numeric counting from 1 to 10. */
 
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+/* 3. Write a program to print multiplication table of any number 
+using for loop. Table number & length should be taken as an 
+input from user.
+ */
+
+let userInput = parseFloat(prompt("Enter a number to  print it's multiplication table"));
+let userInput2 = parseFloat(prompt("Enter length of multiplication table"));
+
+document.write(`<h2>Multiplication Table for ${userInput}</h2>`)
+if () {
+    
+}
+for (let i = 1; i <= userInput2; i++) {
+    let result = userInput * i;
+    document.write(`<h3>${userInput} X ${i} = ${result} </h3>`)
+    
+}
